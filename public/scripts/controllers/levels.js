006bd7a052d8e97565144c333ff4ef38eace8f33
Xtal Osc <xtalosc@gmx.com>
2016-11-18 18:21:56 -0600

    add level list

--- public/scripts/controllers/levels.js
+++ public/scripts/controllers/levels.js
@@ -1,1 +1,19 @@
-console.log("in levels.js");
+myApp.controller("level",["$scope","$http",function($scope,$http){
+  console.log('in level controller');
+
+  $scope.levels =[];
+  $scope.viewLevels = function(){
+    if(verbose){console.log('in viewLevels');};
+
+    //get all levels from database
+    $http({
+      method:'GET',
+      url: '/viewLevels'
+    }).then(function(response){
+      if(verbose){console.log("response from server:",response.data);};
+      $scope.levels=response.data;
+    })//end /viewLevels return
+  };//end viewLevels function
+  $scope.viewLevels();
+
+}]);//end level controller
